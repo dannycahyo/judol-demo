122bc77d0da0de76d1098d3422c70ddcf1f59ee8
dannycahyo <dannydwicahyono@gmail.com>
2025-07-12 09:12:32 +0700

    feat: Add Vercel Analytics for improved tracking and insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { GameProvider } from '../context/GameContext';
+import { Analytics } from '@vercel/analytics/next';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <GameProvider>
       <Component {...pageProps} />
+      <Analytics />
     </GameProvider>
   );
 }
